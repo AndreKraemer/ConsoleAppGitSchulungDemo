51496272e711a41cc8a586c83796cd64b2fe52f7
MartinSchorr <martin.schorr@eplass.com>
2017-05-26 13:03:24 +0200

    Writeline hinzugefügt.

--- ConsoleAppGitSchulung/Program.cs
+++ ConsoleAppGitSchulung/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleAppGitSchulung
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Martin Schorr");
+            Console.ReadLine();
         }
     }
 }
